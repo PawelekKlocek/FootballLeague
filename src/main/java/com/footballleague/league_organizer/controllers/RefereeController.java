3f976ca3c6fe7825f3ae9c99c6e227c8e944b730
Piotr <piotrkloc3k@gmail.com>
2025-02-15 23:09:50 +0100

    League controller, service, repository

---
+++ src/main/java/com/footballleague/league_organizer/controllers/RefereeController.java
@@ -0,0 +1,9 @@
+package com.footballleague.league_organizer.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+
+public class RefereeController {
+}
